7a24db4df3de7d5c0dbbab61a006d73fad097eb4
serapbaysal <serap.baysal@hotmail.com>
2021-10-18 23:25:57 +0300

    created microservices

---
+++ gateway/index.js
@@ -0,0 +1,27 @@
+// required packages
+const express = require("express");
+const dotenv = require("dotenv");
+const cors = require("cors");
+const proxy = require("express-http-proxy");    // !!!!!
+
+
+
+
+// load vars
+dotenv.config({ path: "./.env" });
+
+const PORT = process.env.PORT;
+
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+
+app.use("/orders", proxy("http://localhost:5001"));
+app.use("/costumers", proxy("http://localhost:5002"));
+
+
+app.listen(PORT, () => {
+    console.log(`Server at ${PORT}`);
+})
---
+++ orders/index.js
@@ -0,0 +1,23 @@
+// required packages
+const express = require("express");
+const dotenv = require("dotenv");
+
+
+
+
+// load vars
+dotenv.config({ path: "./.env" });
+
+const PORT = process.env.PORT;
+
+
+const app = express();
+
+app.use("/", (req, res) => {
+    return res.status(200).json({"msg":"Hello from orders"});
+
+})
+
+app.listen(PORT, () => {
+    console.log(`Server at ${PORT}`);
+})
